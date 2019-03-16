27fd22f50b6ca2a333e640e77dc3b9488e3eaeb3
NamChi <namichie@users.noreply.github.com>
2019-03-16 00:39:32 -0400

    Update PreprocessorReuters.java

--- PreprocessorReuters.java
+++ PreprocessorReuters.java
@@ -1,12 +1,7 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
@@ -21,14 +16,12 @@ public class PreprocessorReuters {
 	private final String FILEPATH = System.getProperty("user.dir"); // dir of project saved
 	private final String INPUT = File.separator + "reuters21578.tar"; // name of folder
 	private ArrayList<File> files = new ArrayList<File>();
-	//private final String FILEPATH = System.getProperty("user.dir"); // dir of project saved
-	//private final String INPUT = File.separator + "reuters21578.tar" + File.separator + "reut2-000.sgm";
 
-	protected ArrayList<String> ids, content, topicsList, titlesList, textList, reuters;
+	protected ArrayList<String> content, topicsList, titlesList, textList;
 	protected TreeMap<Integer, String> output;
 	protected File outputFile;
 	private final String OUTPUT = File.separator + "reuters_output.txt";
-	
+
 	public PreprocessorReuters() throws IOException {
 		content = new ArrayList<String>();
 		topicsList = new ArrayList<String>();
@@ -42,16 +35,19 @@ public class PreprocessorReuters {
 		// Get each file from file array
 		for (int i = 0; i < files.size(); i++) {
 			input = files.get(i);
-			
-			System.out.println(input);
-			
+
+			// Parse each file
 			Document doc = Jsoup.parse(input, "UTF-8");
-			
-			Elements r = doc.select("reuters");
-			for (Element t : r) {
-				
-				Elements reut = t.getElementsByTag("reuters");
+
+			// Get each Reuter doc to iterate through
+			Elements reuters = doc.select("reuters");
+			for (Element r : reuters) {
+
+				// Get Reuters doc from its tag, convert to HTML format
+				Elements reut = r.getElementsByTag("reuters");
 				String reuter = reut.html();
+
+				// Cleaning/removing unwanted tags
 				String noDateline = reuter.replaceAll("(?s)<dateline>.*?</dateline>","");
 				String noCompanies = noDateline.replaceAll("(?s)<companies>.*?</companies>","");
 				String noDate = noCompanies.replaceAll("(?s)<date>.*?</date>","");
@@ -60,27 +56,24 @@ public class PreprocessorReuters {
 				String noOrg = noPeople.replaceAll("(?s)<orgs>.*?</orgs>","");
 				String noExchange = noOrg.replaceAll("(?s)<exchanges>.*?</exchanges>","");
 				String noUnknown = noExchange.replaceAll("(?s)<unknown>.*?</unknown>","");
-				
-				
+
+				// Remove title, topic tags since they'll be processed separately
 				String noTitle = noUnknown.replaceAll("(?s)<title>.*?</title>","");
 				String noTopic = noTitle.replaceAll("(?s)<topics>.*?</topics>","");
 
-				//System.out.println(noTopic);
-				
-				//TODO = TOPICS
-				Elements topics = t.getElementsByTag("topics");
-				
+				// Extract topics
+				Elements topics = r.getElementsByTag("topics");
+
 				String topic = topics.text();
 				if (topic.equals("")) {
 					topic = "N/A";
 				} else {
-					topic = topic.replaceAll("<[^>]*>"," ");
+					topic = topic.replaceAll("<[^>]*>"," "); //TODO - separate many topics
 				}
 				topicsList.add(topic);
-				//System.out.println(topic);
-				
-				//TODO - TITLE
-				Elements titles = t.getElementsByTag("title"); 
+
+				// Extract titles
+				Elements titles = r.getElementsByTag("title"); 
 				String title = titles.text();
 				if (title.equals("")) {
 					title = "N/A";
@@ -89,57 +82,48 @@ public class PreprocessorReuters {
 				}
 				titlesList.add(title);
 
-				//System.out.println(title);
-				
-				//TODO - BODY
-				Elements text = t.getElementsByTag("text"); 
+
+				// Extract body
+				Elements text = r.getElementsByTag("text"); 
 				String body = text.text();
 				if (body.equals("")) {
 					body = "N/A";
 				} else {
 					body = text.text();
 				}
-				//System.out.println(body);
-				
 
-				
+
+				// Create a whitelist of tags to ignore when cleaning
 				Whitelist w = new Whitelist();
 				w.addAttributes("text", "body");
-				String test1 = Jsoup.clean(noTopic, w);
-				String test2 = Jsoup.parse(test1).text();
-				//System.out.println(test2);
-				
+				String cleanText = Jsoup.clean(noTopic, w);
+				String finalText = Jsoup.parse(cleanText).text();
+
 				// Remove unwanted text from end of body
-				test2 = test2.replaceAll("Reuter$", "");
-				test2 = test2.replaceAll("Blah blah blah.$", "");
-				test2 = test2.replaceAll("\\*+", "N/A");
-				
-				textList.add(test2);
+				finalText = finalText.replaceAll("Reuter$", "");
+				finalText = finalText.replaceAll("Blah blah blah.$", "");
+				finalText = finalText.replaceAll("\\*+", "N/A");
+
+				textList.add(finalText);
 			}
-			
-			for (int j = 0; j < r.size(); j++) {
-				
-			
-				content.add(topicsList.get(j) + " ééé " + titlesList.get(j) + "---" + textList.get(j));
-				
+
+			// Combine topics + title + body
+			for (int j = 0; j < reuters.size(); j++) {
+
+				content.add(topicsList.get(j) + " Ã©Ã©Ã© " + titlesList.get(j) + "---" + textList.get(j));
 			}
-			
+
+			// Add document to treemap
 			for (int j = 0; j < content.size(); ++j) {
-				
+
 				output.put(j, content.get(j));
 			}
 
 			write(output);
-			}
 
 		}
-	
 
-						
-
-
-
-
+	}
 
 
 
@@ -174,36 +158,6 @@ public class PreprocessorReuters {
 		return textFiles;
 	} // end of function getsgmFileName
 
-	/*
-	 * Code modified from this website:
-	 * https://stackoverflow.com/questions/1384947/java-find-txt-files-in-specified-
-	 * folder
-	 */
-	public File[] getsgmFileName2() {
-
-		// directory of all the .sgm files
-		String directory = FILEPATH + INPUT;
-
-		// create a file instance
-		File file = new File(directory);
-
-		// store all the names of the .sgm files into an ArrayList
-		File[] textFiles = file.listFiles();
-
-		// for each files in the directory
-		for (int i = 0; i < textFiles.length; i++) {
-
-			// if it's a .sgm file, add it to the textFiles ArrayList
-			if (file.getName().endsWith((".sgm"))) {
-				textFiles[i] = file;
-				// array of file path
-				// C:\Users\viet_\Documents\CSI\4TH YR 2018-2019\Winter 2019\CSI4107_Search
-				// Engine\reuters21578.tar\reut2-000.sgm
-			}
-		}
-
-		return textFiles;
-	} // end of function getsgmFileName
 
 	/*
 	 * Site used: https://bukkit.org/threads/saving-loading-hashmap.56447/ Modified
@@ -229,8 +183,6 @@ public class PreprocessorReuters {
 
 		PreprocessorReuters p = new PreprocessorReuters();
 		ArrayList<File> a = p.getsgmFileName();
-
-		System.out.println(a);
 
 	}
 
